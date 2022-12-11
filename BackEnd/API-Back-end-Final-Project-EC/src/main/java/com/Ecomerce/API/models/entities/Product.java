63c758d41f87089b2c57f788460163c5ba887cbd
Phan Hồng Sơn <enochphann@gmail.com>
2022-12-12 06:12:57 +0700

    CRUD Customer

--- BackEnd/API-Back-end-Final-Project-EC/src/main/java/com/Ecomerce/API/models/entities/Product.java
+++ BackEnd/API-Back-end-Final-Project-EC/src/main/java/com/Ecomerce/API/models/entities/Product.java
@@ -1,6 +1,5 @@
 package com.Ecomerce.API.models.entities;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
