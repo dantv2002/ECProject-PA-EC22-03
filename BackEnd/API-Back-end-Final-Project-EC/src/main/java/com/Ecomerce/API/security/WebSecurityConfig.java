dc6938cfe40fc7700b8e9de964cedb38d5d96d34
Phan Hồng Sơn <enochphann@gmail.com>
2022-12-21 14:05:53 +0700

    Update Security

--- BackEnd/API-Back-end-Final-Project-EC/src/main/java/com/Ecomerce/API/security/WebSecurityConfig.java
+++ BackEnd/API-Back-end-Final-Project-EC/src/main/java/com/Ecomerce/API/security/WebSecurityConfig.java
@@ -47,7 +47,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
+		.cors()
+		.and()
+		.csrf().disable()
 		.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
 		.and()
 		.authorizeRequests()
