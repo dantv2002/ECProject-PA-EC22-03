9bde29a962abc43898120e897be5b0bd13166f0e
Văn Dân <74422751+dantv2002@users.noreply.github.com>
2022-12-23 09:05:37 +0700

    Delete FrontEnd directory

--- FrontEnd/src/util/constants/addressApiUrl.js
+++
@@ -1,8 +0,0 @@
-
-export const getDistrictUrl = (cityId) => {
-    return `https://vn-public-apis.fpo.vn/districts/getByProvince?provinceCode=${cityId}&limit=-1`
-}
-
-export const getWardUrl = (districtId) => {
-    return `https://vn-public-apis.fpo.vn/wards/getByDistrict?districtCode=${districtId}&limit=-1`
-}
